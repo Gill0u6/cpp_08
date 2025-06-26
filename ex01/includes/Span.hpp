ec164f7e644d0e9d4028b4445b1df03e1aafe0b6
Alex Gilles <agilles@c1r7p7.42lehavre.fr>
2025-06-26 13:17:23 +0200

    struct & constructor first impl

--- ex01/includes/Span.hpp
+++ ex01/includes/Span.hpp
@@ -1,12 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/26 13:02:16 by agilles           #+#    #+#             */
-/*   Updated: 2025/06/26 13:02:17 by agilles          ###   ########.fr       */
+/*   Created: 2025/06/26 13:02:10 by agilles           #+#    #+#             */
+/*   Updated: 2025/06/26 13:15:44 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <exception>
+
+class Span
+{
+	private:
+		unsigned int		_N;
+		std::vector<int>	_v;
+
+	public:
+		Span();
+		Span(unsigned int N);
+		Span(const Span &cp);
+
+		~Span();
+
+		Span operator=(const Span &cp);
+};
---
+++ ex01/src/Span.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 13:02:08 by agilles           #+#    #+#             */
+/*   Updated: 2025/06/26 13:16:51 by agilles          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Span.hpp"
+
+Span::Span()
+{
+	std::cout << "Span Default Constructor called" << std::endl;
+}
+
+Span::Span(unsigned int N): _N(N)
+{
+	std::cout << "Span _N Constructor called" << std::endl;
+}
+
+Span::Span(const Span &cp)
+{
+	std::cout << "Span Copy Constructor called" << std::endl;
+}
+
+Span::~Span()
+{
+	std::cout << "Span Deconstructor called" << std::endl;
+}
+
+Span Span::operator=(const Span &cp)
+{
+
+}
+
